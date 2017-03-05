83d7e2118bb83bd520e411a35943377d13fbfa98
Romain Moreau <romainmoreau@gmail.com>
2017-03-05 12:12:30 +0100

    Spring Boot upgrade

--- e-paper-web-jssc/src/main/java/fr/romainmoreau/epaper/web/jssc/JsscEPaperProperties.java
+++ e-paper-web-jssc/src/main/java/fr/romainmoreau/epaper/web/jssc/JsscEPaperProperties.java
@@ -4,8 +4,10 @@ import javax.validation.constraints.NotNull;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
 
 @Component
+@Validated
 @ConfigurationProperties("e-paper")
 public class JsscEPaperProperties {
 	@NotNull
