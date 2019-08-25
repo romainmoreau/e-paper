1980d5a960e5f54a8873524c4edfa2c59219654c
Romain Moreau <romainmoreau@gmail.com>
2019-08-25 20:51:54 +0200

    Fix

--- e-paper-web-jserialcomm/src/main/java/fr/romainmoreau/epaper/web/jserialcomm/JSerialCommEPaperProperties.java
+++ e-paper-web-jserialcomm/src/main/java/fr/romainmoreau/epaper/web/jserialcomm/JSerialCommEPaperProperties.java
@@ -3,10 +3,8 @@ package fr.romainmoreau.epaper.web.jserialcomm;
 import javax.validation.constraints.NotNull;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
 import org.springframework.validation.annotation.Validated;
 
-@Component
 @Validated
 @ConfigurationProperties("e-paper")
 public class JSerialCommEPaperProperties {
